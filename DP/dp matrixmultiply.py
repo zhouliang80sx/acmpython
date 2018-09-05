2c45d3e9ed8f772075743995204dc63b9ddc2ebc
Zbc <zbc2008@163.com>
2018-09-05 19:08:56 +0800

    all

--- DP/dp matrixmultiply.py
+++ DP/dp matrixmultiply.py
@@ -23,44 +23,8 @@ A=np.mat(a)
 动态规划经典问题
 https://www.jianshu.com/p/7ffba3910997
 
-https://blog.csdn.net/crazy______/article/details/8263251
-正整数N可以分解成为三个非负实数之和，即N=X+Y+Z, (X,Y,Z>=0),
-求X,Y,Z使得表达式X*(Y+Z^2) 最大，Z^2表示Z的平方。
-
-动态规划问题：添加括号使表达式的值最大
-给定一个表达式（只有加法和乘法两种运算），要求通过添加括号使得表达式的值最大
-譬如说100×100+100×100×100+100×100的最大值是(100×(100+100))×((100×(100+100))×100)=40000000000
- 
-设原计算式为: n1 op1 n2 op2 n3 op3 ... n[m-1] op[m-1] n[m]. 其中n为数字, op为操
-作符(即+或*)
-另设T(n[i], n[j])为第i个数和第j个数之间的子式的最大值. 则有
-T(n[i], n[j]) = max{T(n[i], n[m]) op[m] T(n[m+1], n[j])} 其中m from i to j.
-
-https://wenku.baidu.com/view/7b9ed516a2161479171128a1.html
 
 
-动态规划算法例题及解析
-https://blog.csdn.net/runninglion/article/details/17338409
-注意两个负数相乘的结果是正数，故它们可能大于只用正数求解的结果，
-但是只有做运算的两负数都尽可能的小，才能使结果尽可能的大。
-为了解决表达式中有负数的问题，需要多加一个 dp矩阵，用于保存最大值和最小值。
-
-
-https://segmentfault.com/a/1190000003939663
-给定一个整数数组，要求在数字之间任意添加乘号，加号和括号，使得最后表达式结果最大。
-比如1121,最大值为(1+1)*(2+1)，这里数字可以是0或者负数
-解法和I是一样的，不过这里我们要维护一个最大表和一个最小表，这样，每次我们要乘的那
-个数是正数时，我们的最大值就是之前的最大值乘以这个正数，最小值就是之前的最小值乘
-以这个正数。如果要乘的是个负数的话，我们的最大值就是之前的最小值乘以这个正数
-最小值就是之前的最大值乘以这个正数。另外，我们还要先初始化这个两个表，
-因为之前那题结果肯定大于0，所以我们不用初始化，
-不管怎么算原先矩阵中的0都会被替换掉。而本题中可以有0和负数，
-所以我们要把最大表先初始化为负最大值，最小表初始化为正最小值。
-
-
-
-https://blog.csdn.net/u010372095/article/details/46573329
-CF E. Vanya and Brackets(添加一对括号使得表达式的值最大)
 
 
 应用动态规划方法
@@ -79,6 +43,55 @@ CF E. Vanya and Brackets(添加一对括号使得表达式的值最大)
 
 
 '''
+
+
+def matrixmul(A):
+    n=len(A)
+    M=[[0 for i in range(n)] for j in range(n)]
+    #M[i][j]:Ai.....Aj, 最少乘法次数
+    K=[[-1 for i in range(n)] for j in range(n)]
+    #k[j][j]:(Ai...Ak)||(Ak+1..Aj), 的最少乘法次数的划分位置
+
+    for step in range(1,n):
+        for i in range(0,n):
+            j=i+step        
+            if j>=n: continue
+            minValue=1000000000
+            for k in range(i,j):
+                M[i][j]=M[i][k]+M[k+1][j]+A[i][0]*A[k][1]*A[j][1]
+                if M[i][j]<minValue:
+                    minValue=M[i][j]
+                    K[i][j]=k
+            M[i][j]=minValue # 取k种划分的最小值
+           
+        print('\n','step=',step)
+        for i in range(len(M)):
+            for j in range(len(M[0])):
+                print('{:8d}'.format(M[i][j]),end=',')
+            print()
+            
+    return M,K
+    
+def printrt(i,j): #print result 
+    if i==j:
+        print('A[',i,']',sep='',end='')
+        return
+    
+    split=K[i][j]
+    print('(',end='')
+    printrt(i,split)
+    #.....
+    printrt(split+1,j)
+    print(')',end='')    
+    
+    
+     
+A=[(3,5),(5,10),(10,8),(8,2),(2,4)]
+M,K=matrixmul(A)    
+printrt(0,len(A)-1)
+
+
+
 
 
 
